7deccecf8ad325f26dcde847fa8da4f79d0051c3
Joshua_Arfa <soondevjosh@gmail.com>
2023-03-07 18:14:25 -0600

    adding projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,15 +12,33 @@ const Portfolio = () => {
       {/* py-16 px-4 */}
         <div>
           <h2 className="font-bold text-2xl text-center py-8"> Vanilla Js </h2>
-          <a href="/" className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <p className="py-2 border-b mx-8 "> ➡️ TODO_list </p>
+          <a
+            target='_blank' 
+            href="https://to-do-app-jsss.netlify.app" 
+            className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            rel="noreferrer"
+          >
+            <p className="py-2 border-b mx-8 "> ➡️ TODO_list: using js DOM and local-storage </p>
           </a>
         </div>
 
         <div>
           <h2 className="font-bold text-2xl text-center py-8"> React</h2>
-          <a href="/" className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <p className="py-2 border-b mx-8 "> ➡️ Tic Tac Toe: In this one i used hooks </p>
+          <a 
+            target="_blank"
+            href="https://tict-tact-toe.netlify.app" 
+            className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            rel="noreferrer"
+          >
+            <p className="py-2 border-b mx-8 "> ➡️ Tic Tac Toe: In this one i used the state (hook) to control the players, this a simple game. </p>
+          </a>
+          <a
+            target="_blank" 
+            href="https://gif-appreact.netlify.app"
+            className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            rel="noreferrer"
+          >
+            <p className="py-2 border-b mx-8 "> ➡️ Gif app: In this one i used the state (hook) I used the GIPHY Devlopers Api. Here you can search your favorites gifs. </p>
           </a>
         </div>
 
@@ -34,14 +52,27 @@ const Portfolio = () => {
         <div>
           <h2 className="font-bold text-2xl text-center py-8 "> NodeJs</h2>
           <a href="/" className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <p className="py-2 border-b mx-8 "> ➡️ TODO_list </p>
+            <p className="py-2 border-b mx-8 "> ➡️ Favorite links: this one uses Js, nodejs, and mysql. Here you can log in to the web app and save your favorite links (every user has their own session) </p>
           </a>
         </div>
         
         <div>
           <h2 className="font-bold text-2xl text-center py-8 "> HTML, CSS & JS</h2>
-          <a href="/" className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
-            <p className="py-2 border-b mx-8 "> ➡️ Website whit grid </p>
+          <a 
+            target="_blank"
+            href="https://web-mas-carrito.netlify.app" 
+            className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            rel="noreferrer"
+          >
+            <p className="py-2 border-b mx-8 "> ➡️Beef Website whit good practices (css) and a shopping cart  </p>
+          </a>
+          <a 
+            target="_blank"
+            href="https://coffeewebs.netlify.app" 
+            className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            rel="noreferrer"
+          >
+            <p className="py-2 border-b mx-8 "> ➡️Coffee Website using some effects </p>
           </a>
         </div>
 
